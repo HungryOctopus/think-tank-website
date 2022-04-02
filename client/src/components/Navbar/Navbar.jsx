f70268540794278687375e30e578c17f655409b6
Amélie Monnoyer <49373560+HungryOctopus@users.noreply.github.com>
2022-04-02 14:54:35 +0200

    responsive navbar

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,8 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import './Navbar.css';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBars } from '@fortawesome/free-solid-svg-icons';
+import logo from './logo-thinktank-white.png';
 
 function Navbar() {
   const [click, setClick] = useState(false);
@@ -10,28 +9,27 @@ function Navbar() {
   const closeMobileMenu = () => setClick(false);
 
   return (
-    <>
-      <nav className="sticky top-0 shadow">
-        <div className="navbar-container flex flex-row">
-          <div className="navbar-logo basis-1/4 content-center p-3">
-            <Link to="/" className="navbar-logo" onClick={closeMobileMenu}>
-              <h2>Test2</h2>
-            </Link>
-          </div>
-          <div className="navbar-menu basis-3/4 flex justify-end items-center px-3">
-            <Link to="/projects" className="px-3" onClick={closeMobileMenu}>
-              <h3>Projects</h3>
-            </Link>
-            <Link to="/about" className="px-3" onClick={closeMobileMenu}>
-              <h3>About us</h3>
-            </Link>
-            <div className="language-selection px-5">
-              <h3>EN</h3>
-            </div>
+    <nav className="sticky top-0 shadow">
+      <div className="navbar-container flex flex-row justify-between">
+        <div className="nav-header">
+          <img src={logo} alt="think tank logo white" />
+        </div>
+
+<div className="navbar-menu flex justify-end items-center px-3">
+          <Link to="/projects" className="px-3" onClick={closeMobileMenu}>
+            <h3>Projects</h3>
+          </Link>
+          <Link to="/about" className="px-3" onClick={closeMobileMenu}>
+            <h3>About us</h3>
+          </Link>
+          <div className="language-selection px-5">
+            <h3>EN</h3>
           </div>
         </div>
-      </nav>
-    </>
+
+        
+      </div>
+    </nav>
   );
 }
 
