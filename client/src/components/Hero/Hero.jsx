e74e0177927a1a021970855f572a2c8afe9b3718
Amélie Monnoyer <49373560+HungryOctopus@users.noreply.github.com>
2022-04-03 15:32:57 +0200

    styling hero component

--- client/src/components/Hero/Hero.jsx
+++ client/src/components/Hero/Hero.jsx
@@ -1,15 +1,13 @@
 import React, { useState, useEffect } from 'react';
-//import industryPic from './../images/hero-thinktank-1.jpeg';
 import './Hero.css';
 
 function Hero() {
   return (
     <div className="hero-container">
       <div className="claim py-4">
-        {/* <img src={industryPic} alt="industry" /> */}
-        <h1 className="text-7xl text-white">Engineering the future</h1>
+        <h1 className="text-white">Engineering the future</h1>
         {/* Gradient on text? */}
-        <h1 className="text-7xl text-sblue">together</h1>
+        <h2 className="text-sblue">together</h2>
       </div>
     </div>
   );
